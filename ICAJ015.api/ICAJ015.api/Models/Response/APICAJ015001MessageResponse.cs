4536a550bfd6067f36954fc3d9c244e94b954225
Sol281987 <solmonca_28@hotmail.com>
2021-04-07 20:23:07 -0500

    Add files via upload

---
+++ ICAJ015.api/ICAJ015.api/Models/Response/APICAJ015001MessageResponse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ICAJ015.api.Models.Response
+{
+    public class APICAJ015001MessageResponse
+    {
+        public Guid SesionId { get; set; }
+        public string DescriptionId { get; set; }
+        public string[] ErrorList { get; set; }
+        public List<APFinancialPlanICAJ015001> FinancialPlanList { get; set; }
+    }
+}
