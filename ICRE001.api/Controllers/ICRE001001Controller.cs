2a2910d8586028a0614d50f85a3b5cb6e3a8bc40
Sol Moncada <solmonca_28@hotmail.com>
2021-04-07 20:49:58 -0500

    Agregar archivos de proyecto.

---
+++ ICRE001.api/Controllers/ICRE001001Controller.cs
@@ -0,0 +1,77 @@
+﻿using Azure.Messaging.ServiceBus;
+using ICRE001.api.Infraestructure.Configuration;
+using ICRE001.api.Infraestructure.Services;
+using ICRE001.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ICRE001.api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ICRE001001Controller : ControllerBase
+    {
+        private IManejadorRequest<APICRE001001MessageRequest> QueueRequest;
+        private IManejadorResponse<APICRE001001MessageResponse> QueueResponse;
+        private static RegistroLog Logger = new RegistroLog();
+        private static readonly Configuracion conf = new Configuracion();
+        static IConfigurationRoot configuracion = conf.GetConfiguration();
+        private static string vl_ConnectionStringRequest = Convert.ToString(configuracion.GetSection("Data").GetSection("ConectionStringRequest").Value);
+        private static string vl_ConnectionStringResponse = Convert.ToString(configuracion.GetSection("Data").GetSection("ConectionStringResponse").Value);
+        private static string vl_QueueRequest = configuracion.GetSection("Data").GetSection("QueueRequest").Value.ToString();
+        private static string vl_QueueResponse = configuracion.GetSection("Data").GetSection("QueueResponse").Value.ToString();
+        private static int vl_Time = Convert.ToInt32(configuracion.GetSection("Data").GetSection("TimeSleep").Value);
+
+
+        public ICRE001001Controller(IManejadorRequest<APICRE001001MessageRequest> _manejadorRequest, IManejadorResponse<APICRE001001MessageResponse> _manejadorReponse)
+        {
+            QueueRequest = _manejadorRequest;
+            QueueResponse = _manejadorReponse;
+        }
+
+
+
+
+        // GET: api/<ValuesController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<APICRE001001MessageResponse>> ICRE001001(APICRE001001MessageRequest parametrorequest)
+        {
+            try
+            {
+                APICRE001001MessageResponse respuesta = null;
+                string sesionid = Guid.NewGuid().ToString();
+                parametrorequest.SesionId = sesionid;
+
+                await QueueRequest.EnviarMensajeAsync(sesionid, vl_ConnectionStringRequest, vl_QueueRequest, parametrorequest);
+
+                respuesta = await QueueResponse.RecibeMensajeSesion(vl_ConnectionStringResponse, vl_QueueResponse, sesionid, 1, 3);
+
+
+                return Ok(respuesta);
+            }
+            catch (Exception ex)
+            {
+                Logger.FileLogger("APICRE001001", ex.ToString());
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+
+            }
+
+        }
+
+    }
+}
