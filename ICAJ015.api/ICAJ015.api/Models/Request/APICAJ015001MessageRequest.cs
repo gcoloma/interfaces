d57fc0e2165ea721790043b82b7264549799f91f
Sol281987 <solmonca_28@hotmail.com>
2021-04-07 20:17:08 -0500

    Add files via upload

---
+++ ICAJ015.api/ICAJ015.api/Models/Request/APICAJ015001MessageRequest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ICAJ015.api.Models
+{
+    public class APICAJ015001MessageRequest
+    {
+        public string DataAreaId { get; set; }
+        public string Enviroment { get; set; }       
+        //public Guid SesionId { get; set; }
+        public string SesionId { get; set; }        
+        public string BusinessUnit { get; set; }
+        public string Channel { get; set; }
+        public DateTime TransDate { get; set; }
+
+    }
+}
