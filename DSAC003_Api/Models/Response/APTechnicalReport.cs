ae93b1cf2bb73b709f1c70a11741fee851a6425e
JuanPabloMolina <juan.molnav83@gmail.com>
2021-04-09 15:55:19 -0500

    Juan Pablo Molina Interfaz DSAC003
    Juan Pablo Molina Interfaz DSAC003

---
+++ DSAC003_Api/Models/Response/APTechnicalReport.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DSAC003.Api.Models.Response
+{
+    //Class APTechnicalReport (Proveedores) / (APDSAC003001MessageResponse)
+    public class APTechnicalReport
+    {
+        [Required]
+        [StringLength(20)]
+        public string WorkOrderNumber { get; set; }
+
+        public string AssetNumber { get; set; }
+
+        public string ItemCode { get; set; }
+
+        public string SerialNumber { get; set; }
+
+        public string ReportType { get; set; }
+
+        public string Discount { get; set; }
+        
+    }
+}
+
+
