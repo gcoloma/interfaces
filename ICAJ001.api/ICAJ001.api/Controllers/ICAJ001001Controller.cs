5bd804c5645b4143184c376093556219189173fe
Enrique Calle <enrique_mcp@outlook.com>
2021-06-10 07:20:21 -0500

    Interfaz - Enrique Calle

---
+++ ICAJ001.api/ICAJ001.api/Controllers/ICAJ001001Controller.cs
@@ -0,0 +1,83 @@
+﻿using ICAJ001.api.Infraestructura.Servicios;
+using ICAJ001.api.Infraestructure.Configuration;
+using ICAJ001.api.Infraestructure.Services;
+using ICAJ001.api.Models._001.Response;
+using ICAJ001.api.Models.ResponseHomologacion;
+using ICAJ001.Models._001.Request;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ICAJ001.api.Controllers
+{
+    
+    [ApiController]
+    public class ICAJ001001Controller : ControllerBase
+    {
+        private IManejadorRequest<APICAJ001001MessageRequest> QueueRequest;
+        private IManejadorResponse<APICAJ001001MessageResponse> QueueResponse;
+        private IManejadorHomologacion<ResponseHomologacion> homologacionRequest;
+        private static RegistroLog Logger = new RegistroLog();
+        private static readonly Configuracion conf = new Configuracion();
+        static IConfigurationRoot configuracion = conf.GetConfiguration();
+        private static string vl_ConnectionStringRequest = Convert.ToString(configuracion.GetSection("Data").GetSection("ConectionStringRequest").Value);
+        private static string vl_ConnectionStringResponse = Convert.ToString(configuracion.GetSection("Data").GetSection("ConectionStringResponse").Value);
+        private static string vl_QueueRequest = configuracion.GetSection("Data").GetSection("QueueRequest").Value.ToString();
+        private static string vl_QueueResponse = configuracion.GetSection("Data").GetSection("QueueResponse").Value.ToString();
+        private static int vl_Time = Convert.ToInt32(configuracion.GetSection("Data").GetSection("TimeSleep").Value);
+        private static string vl_Environment = Convert.ToString(configuracion.GetSection("Data").GetSection("ASPNETCORE_ENVIRONMENT").Value);
+        private static string sbUriHomologacionDynamic = Convert.ToString(configuracion.GetSection("Data").GetSection("UriHomologacionDynamicSiac").Value);
+        private static string sbMetodoWsUriSiac = Convert.ToString(configuracion.GetSection("Data").GetSection("MetodoWsUriSiac").Value);
+        private static string sbMetodoWsUriAx = Convert.ToString(configuracion.GetSection("Data").GetSection("MetodoWsUriAx").Value);
+
+        public ICAJ001001Controller(IManejadorRequest<APICAJ001001MessageRequest> _manejadorRequest
+             , IManejadorResponse<APICAJ001001MessageResponse> _manejadorReponse, IManejadorHomologacion<ResponseHomologacion> _homologacionRequest)
+        {
+            QueueRequest = _manejadorRequest;
+            QueueResponse = _manejadorReponse;
+            homologacionRequest = _homologacionRequest;
+        }
+
+
+        [HttpPost]
+        [Route("APICAJ001001")]
+        public async Task<ActionResult<APICAJ001001MessageResponse>> APICAJ001001(APICAJ001001MessageRequest parametrorequest)
+        {
+            try
+            {
+                string DataAreaId = parametrorequest.DataAreaId;
+                ResponseHomologacion ResultadoHomologa = await homologacionRequest.GetHomologacion(sbUriHomologacionDynamic, sbMetodoWsUriSiac, DataAreaId);
+                // mapear resultado homologacion
+                parametrorequest.DataAreaId = ResultadoHomologa?.Response ?? "0001";
+
+                APICAJ001001MessageResponse respuesta = null;
+                string sesionid = Guid.NewGuid().ToString();
+                parametrorequest.SessionId = sesionid;
+                parametrorequest.Enviroment = vl_Environment;
+
+                await QueueRequest.EnviarMensajeAsync(sesionid, vl_ConnectionStringRequest, vl_QueueRequest, parametrorequest);
+
+                respuesta = await QueueResponse.RecibeMensajeSesion(vl_ConnectionStringResponse, vl_QueueResponse, sesionid, 1, 3);
+
+                if (respuesta == null)
+                {
+                    Logger.FileLogger("APICAJ001001", "No se retorno resultado de Dynamics");
+                    return StatusCode(StatusCodes.Status204NoContent, "No se retorno resultado de dynamics");
+                }
+
+                return Ok(respuesta);
+            }
+            catch (Exception ex)
+            {
+                Logger.FileLogger("APICAJ001001", ex.ToString());
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+
+            }
+
+        }
+    }
+}
---
+++ ICAJ001.api/ICAJ001.api/Models/001/Response/APICAJ001001MessageResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ICAJ001.api.Models._001.Response
+{
+    public class APICAJ001001MessageResponse
+    {
+        public List<APBankAccountTable> BankAccountTableList { get; set; }
+        public bool StatusId { get; set; }
+      //  public List<string> ErrorList { get; set; }
+    }
+}
